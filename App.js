54858af0407194518587e0e0daa64b8b12d0bc2b
Sandhra <sandhra.mary@keyvalue.systems>
2024-05-26 16:07:14 +0530

    React code in js

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("h1", {}, "Hello Word from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const newElements = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "H1 fro child1"),
+    React.createElement("h2", {}, "HH2 from child1"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "H1 fro child1"),
+    React.createElement("h2", {}, "HH2 from child1"),
+  ]),
+]);
+root.render(newElements);
