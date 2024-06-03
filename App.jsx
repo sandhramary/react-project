c36da18e8545231957ac4b6ff715b3344a85f537
Sandhra <sandhra.mary@keyvalue.systems>
2024-06-03 15:02:33 +0530

    Basic food order app ui

---
+++ App.jsx
@@ -0,0 +1,360 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+const list = [
+  {
+    info: {
+      id: "704921",
+      name: "Chinese Wok",
+      cloudinaryImageId: "e0839ff574213e6f35b3899ebf1fc597",
+      locality: "Ravipuram",
+      areaName: "M G Road",
+      costForTwo: "₹250 for two",
+      cuisines: ["Chinese", "Asian", "Tibetan", "Desserts"],
+      avgRating: 4.2,
+      parentId: "61955",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 41,
+        lastMileTravel: 7.4,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "7.4 km",
+        iconType: "ICON_TYPE_EMPTY"
+      },
+      availability: {
+        nextCloseTime: "2024-06-04 01:00:00",
+        opened: true
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {}
+        }
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹179"
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {}
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {}
+        }
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {}
+    },
+    analytics: {
+      context: "seo-data-d933eddf-34b8-4c02-8434-4add5ab7c110"
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/chinese-wok-ravipuram-m-g-road-kochi-704921",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK"
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+    info: {
+      id: "240093",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Fort Kochi",
+      areaName: "Fort Kochi",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.3,
+      parentId: "721",
+      avgRatingString: "4.3",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 36,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY"
+      },
+      availability: {
+        nextCloseTime: "2024-06-04 01:00:00",
+        opened: true
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Pizza.png",
+            description: "Delivery!"
+          }
+        ]
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Pizza.png"
+                }
+              }
+            ]
+          },
+          textBased: {},
+          textExtendedBadges: {}
+        }
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {}
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {}
+        }
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {}
+    },
+    analytics: {
+      context: "seo-data-d933eddf-34b8-4c02-8434-4add5ab7c110"
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/pizza-hut-fort-kochi-kochi-240093",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK"
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+    info: {
+      id: "89572",
+      name: "UBQ by Barbeque Nation",
+      cloudinaryImageId: "rrh9bulv6vcj4ebduth7",
+      locality: "Kacheripadi",
+      areaName: "Kacheripadi",
+      costForTwo: "₹300 for two",
+      cuisines: [
+        "North Indian",
+        "Barbecue",
+        "Biryani",
+        "Kebabs",
+        "Mughlai",
+        "Desserts"
+      ],
+      avgRating: 4,
+      parentId: "10804",
+      avgRatingString: "4.0",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 62,
+        lastMileTravel: 10.5,
+        serviceability: "SERVICEABLE",
+        slaString: "60-65 mins",
+        lastMileTravelString: "10.5 km",
+        iconType: "ICON_TYPE_EMPTY"
+      },
+      availability: {
+        nextCloseTime: "2024-06-03 16:30:00",
+        opened: true
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {}
+        }
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹199"
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {}
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {}
+        }
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {}
+    },
+    analytics: {
+      context: "seo-data-d933eddf-34b8-4c02-8434-4add5ab7c110"
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/ubq-by-barbeque-nation-kacheripadi-kochi-89572",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK"
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+    info: {
+      id: "301957",
+      name: "Barbeque Nation",
+      cloudinaryImageId: "yv7fq1ypyq9eht2rsafk",
+      locality: "Kacheripadi",
+      areaName: "Kacheripadi",
+      costForTwo: "₹600 for two",
+      cuisines: ["Barbecue", "Biryani", "Kebabs", "Mughlai", "Desserts"],
+      avgRating: 4.1,
+      parentId: "2438",
+      avgRatingString: "4.1",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 61,
+        lastMileTravel: 10.5,
+        serviceability: "SERVICEABLE",
+        slaString: "60-65 mins",
+        lastMileTravelString: "10.5 km",
+        iconType: "ICON_TYPE_EMPTY"
+      },
+      availability: {
+        nextCloseTime: "2024-06-03 16:30:00",
+        opened: true
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {}
+        }
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹125 OFF",
+        subHeader: "ABOVE ₹449",
+        discountTag: "FLAT DEAL"
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {}
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {}
+        }
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {}
+    },
+    analytics: {
+      context: "seo-data-d933eddf-34b8-4c02-8434-4add5ab7c110"
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/barbeque-nation-kacheripadi-kochi-301957",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK"
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  }
+];
+
+const Header = () => (
+  <div className="header">
+    <div className="logo-container">
+      <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&txt_keyword=All" />
+    </div>
+    <div className="nav-items">
+      <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Contact us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  </div>
+);
+
+const RestaurantCard = (props) => {
+  const { resDetails } = props;
+  const { name, cuisines, costForTwo, cloudinaryImageId } = resDetails?.info;
+
+  return (
+    <div className="restaurant-card">
+      <div className="restaurant-img">
+        <img
+          src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
+          alt="img"
+        />
+      </div>
+      <h3>{name}</h3>
+      <h4>{cuisines?.join(", ")}</h4>
+      <h4>{costForTwo}</h4>
+    </div>
+  );
+};
+
+const Body = () => (
+  <>
+    <div>Search</div>
+    <div className="restaurant-container">
+      {list.map((item) => (
+        <RestaurantCard resDetails={item} />
+      ))}
+    </div>
+  </>
+);
+
+const Footer = () => (
+  <div className="footer">
+    <div>Copyright</div>
+    <div>Links</div>
+  </div>
+);
+
+const AppLayout = () => (
+  <div id="app">
+    <Header />
+    <Body />
+    <Footer />
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
